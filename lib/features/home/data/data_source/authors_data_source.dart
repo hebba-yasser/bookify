5beb05023eb04d4c9d47af9f8a5c4f0a007d9ae8
hebbayasser <hebbayasser1234@gmail.com>
2025-03-30 23:02:11 +0200

    updated the data in authors_data_source.dart

--- lib/features/home/data/data_source/authors_data_source.dart
+++ lib/features/home/data/data_source/authors_data_source.dart
@@ -13,44 +13,38 @@ class AuthorDataSource {
           'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1596216614i/1077326._UX200_CR0,0,200,200_.jpg',
     ),
     AuthorModel(
-      authorName: 'Michelle Obama',
-      authorImagePath:
-          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1492817699i/4156583._UX200_CR0,0,200,200_.jpg',
-    ),
+        authorName: 'Michelle Obama',
+        authorImagePath:
+            'https://www.shutterstock.com/image-photo/bangkok-jan-29-waxwork-michelle-260nw-371005931.jpg'),
     AuthorModel(
-      authorName: 'Yuval Noah Harari',
-      authorImagePath:
-          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1441253583i/1049806._UX200_CR0,0,200,200_.jpg',
-    ),
+        authorName: 'Malcolm Gladwell',
+        authorImagePath:
+            'https://dev.speakersla.com/wp-content/uploads/2015/03/Malcolm-Gladwell.jpg'),
     AuthorModel(
-      authorName: 'Brandon Sanderson',
-      authorImagePath:
-          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1603787120i/38550._UX200_CR0,0,200,200_.jpg',
-    ),
+        authorName: 'Brandon Sanderson',
+        authorImagePath:
+            'https://yt3.googleusercontent.com/nfuxYKdKi_Y6cVrE3uc8PT3unSqgcL3eekHvnQfSBbhry4P31-yNO7O2Qs-d82aIxZ9uHiqe5g=s900-c-k-c0x00ffffff-no-rj'),
     AuthorModel(
       authorName: 'Colleen Hoover',
       authorImagePath:
-          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1494961103i/5430144._UX200_CR0,0,200,200_.jpg',
+          'https://tse2.mm.bing.net/th?id=OIP.sSbSH7dsrRdHTVGuwgdwBwHaHY&pid=Api',
     ),
     AuthorModel(
       authorName: 'James Clear',
       authorImagePath:
-          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1535665445i/13882207._UX200_CR0,0,200,200_.jpg',
+          'https://tse2.mm.bing.net/th?id=OIP.s5YZ1W09nV9bYsuIgvWGLwHaHa&pid=Api',
     ),
     AuthorModel(
       authorName: 'Agatha Christie',
       authorImagePath:
-          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1589996526i/123715._UX200_CR0,0,200,200_.jpg',
+          'https://tse2.mm.bing.net/th?id=OIP.wMNxHOxfMFowtOhbb3hfpgHaIn&pid=Api',
     ),
     AuthorModel(
-      authorName: 'Dan Brown',
-      authorImagePath:
-          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1362764110i/630._UX200_CR0,0,200,200_.jpg',
-    ),
+        authorName: 'Dan Brown',
+        authorImagePath: 'https://images4.penguinrandomhouse.com/author/3446'),
     AuthorModel(
-      authorName: 'Margaret Atwood',
-      authorImagePath:
-          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1282855773i/3472._UX200_CR0,0,200,200_.jpg',
-    ),
+        authorName: 'Margaret Atwood',
+        authorImagePath:
+            'https://naccnaca-biographies.s3.amazonaws.com/30651/2012_-_margaret_atwood_-_photo_by_jean_malek.jpg'),
   ];
 }
