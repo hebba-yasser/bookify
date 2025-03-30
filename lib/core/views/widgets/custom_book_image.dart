9441e274fcf7b9e76659a09604043acbfb80cee1
hebbayasser <hebbayasser1234@gmail.com>
2025-03-30 23:08:32 +0200

    handled error when url is not right

--- lib/core/views/widgets/custom_book_image.dart
+++ lib/core/views/widgets/custom_book_image.dart
@@ -1,20 +1,24 @@
-import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({Key? key, required this.image}) : super(key: key);
   final String image;
   @override
   Widget build(BuildContext context) {
+    bool isValidUrl = Uri.tryParse(image)?.hasAbsolutePath ?? false;
+
     return AspectRatio(
       aspectRatio: 2.4 / 3,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(12),
-        child:
-        CachedNetworkImage(
-          fit: BoxFit.fill,
-          imageUrl: image,
-          errorWidget: (context, url, error) => Icon(Icons.error),
-        ),
+        child: isValidUrl
+            ? CachedNetworkImage(
+                fit: BoxFit.fill,
+                imageUrl: image,
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              )
+            : Icon(Icons.error),
       ),
     );
   }
