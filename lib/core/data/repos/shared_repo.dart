174ba965b98e018ffcba47f4407808a628f180b4
hebbayasser <hebbayasser1234@gmail.com>
2025-04-01 01:12:38 +0200

    update shared repo and implement update recently viewed

--- lib/core/data/repos/shared_repo.dart
+++ lib/core/data/repos/shared_repo.dart
@@ -2,6 +2,7 @@ import 'package:bookify/core/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../failure/failure.dart';
+import '../models/user_model/small_book_model.dart';
 import '../models/user_model/user_model.dart';
 
 abstract class SharedRepo {
@@ -10,4 +11,7 @@ abstract class SharedRepo {
   Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
       {required String category});
   Stream<Either<Failure, UserModel>> fetchUserData({required String id});
+  Future<Either<Failure, void>> updateUserRecentlyViewedList({
+    required SmallBookModel book,
+  });
 }
--- lib/core/data/repos/shared_repo_imp.dart
+++ lib/core/data/repos/shared_repo_imp.dart
@@ -1,4 +1,5 @@
 import 'package:bookify/core/data/models/book_model/book_model.dart';
+import 'package:bookify/core/data/models/user_model/small_book_model.dart';
 import 'package:bookify/core/data/repos/shared_repo.dart';
 import 'package:bookify/core/failure/failure.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,6 +7,7 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../../../constants.dart';
 import '../../utils/api_service.dart';
 import '../models/user_model/user_model.dart';
 
@@ -68,6 +70,46 @@ class SharedRepoImp implements SharedRepo {
         yield Left(FirebaseFailure.fromFirebaseAuthException(e));
       } else {
         yield Left(FirebaseFailure(e.toString()));
+      }
+    }
+  }
+
+  Future<Either<Failure, void>> updateUserRecentlyViewedList({
+    required SmallBookModel book,
+  }) async {
+    try {
+      final userDoc = FirebaseFirestore.instance.collection('users').doc(uId);
+
+      // Get the current snapshot
+      final snapshot = await userDoc.get();
+      List<Map<String, dynamic>> currentList = List<Map<String, dynamic>>.from(
+          snapshot.data()?['recentlyViewed'] ?? []);
+
+      // Remove the book if it already exists (avoid duplicates)
+      currentList.removeWhere((item) => item['id'] == book.id);
+
+      // Add the new book with timestamp at the start
+      currentList.insert(0, {
+        ...book.toJson(),
+        'viewedAt': Timestamp.now(),
+      });
+
+      // Keep only the 10 most recent items
+      if (currentList.length > 10) {
+        currentList = currentList.sublist(0, 10);
+      }
+
+      // Update Firestore
+      await userDoc.set({
+        'recentlyViewed': currentList,
+      }, SetOptions(merge: true));
+
+      return const Right(null);
+    } catch (e) {
+      if (e is FirebaseAuthException) {
+        return left(FirebaseFailure.fromFirebaseAuthException(e));
+      } else {
+        return left(FirebaseFailure(e.toString()));
       }
     }
   }
