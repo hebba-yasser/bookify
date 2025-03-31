4176bfab6d2161f6becff9ff9f1e3f81cf3f86d5
hebbayasser <hebbayasser1234@gmail.com>
2025-03-31 08:52:23 +0200

    search features with ui Done -consumed user data cubit to fetch recently viewed

--- lib/features/search/presentation/views/widgets/resent_search_section.dart
+++ lib/features/search/presentation/views/widgets/resent_search_section.dart
@@ -1,7 +1,14 @@
+import 'package:bookify/core/utils/functions/Navigaor_push.dart';
+import 'package:bookify/features/search/presentation/views/search_result_view.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../../../../core/cubits/user_data_cubit/user_data_cubit.dart';
+import '../../../../../core/cubits/user_data_cubit/user_data_state.dart';
 import '../../../../../core/styles/fonts.dart';
+import '../../../../../core/utils/functions/custom_loading_indicator.dart';
 import 'list_builder_item.dart';
 
 class ResentSearchSection extends StatelessWidget {
@@ -11,34 +18,54 @@ class ResentSearchSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Recent Search',
-            style: AppFonts.caption,
-          ),
-          Expanded(
-            child: ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return ListBuilderItem(
-                  onpressed: () {},
-                  text: 'search esult ',
-                  leading: const Icon(
-                    FontAwesomeIcons.magnifyingGlass,
-                    color: Colors.grey,
-                    size: 16,
+    return BlocBuilder<UserDataCubit, UserDataState>(
+      builder: (context, userState) {
+        if (userState is UserDataFailure) {
+          return Text(userState.errMessage);
+        } else if (userState is UserDataSuccess) {
+          return ConditionalBuilder(
+            condition: userState.user.recentSearch!.isEmpty,
+            builder: (context) => const SizedBox(),
+            fallback: (context) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Recent Search',
+                    style: AppFonts.caption,
                   ),
-                );
-              },
+                  Expanded(
+                    child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: userState.user.recentSearch?.length,
+                      itemBuilder: (context, index) {
+                        return ListBuilderItem(
+                          onpressed: () {
+                            navigatorPush(
+                                context,
+                                SearchResultView(
+                                    Query:
+                                        userState.user.recentSearch![index]));
+                          },
+                          text: userState.user.recentSearch![index],
+                          leading: const Icon(
+                            FontAwesomeIcons.magnifyingGlass,
+                            color: Colors.grey,
+                            size: 16,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
+          );
+        } else {
+          return customLoadingIndicator();
+        }
+      },
     );
   }
 }
