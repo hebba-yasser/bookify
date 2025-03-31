0a3fdca3cd23b76b8919d6b430cf0f20398224fa
hebbayasser <hebbayasser1234@gmail.com>
2025-04-01 01:46:38 +0200

    fix in login -to fetch data of user right after login not fetch previous use data

--- lib/features/auth/presentation/views/widgets/sign_in_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_view_body.dart
@@ -3,7 +3,11 @@ import 'package:conditional_builder_null_safety/conditional_builder_null_safety.
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../constants.dart';
+import '../../../../../core/cubits/user_data_cubit/user_data_cubit.dart';
+import '../../../../../core/cubits/user_data_cubit/user_data_state.dart';
 import '../../../../../core/styles/fonts.dart';
+import '../../../../../core/utils/cache_helper.dart';
 import '../../../../../core/utils/functions/Navigaor_push.dart';
 import '../../../../../core/utils/functions/custom_loading_indicator.dart';
 import '../../../../../core/utils/functions/custom_show_toast.dart';
@@ -29,102 +33,110 @@ class _SignInViewBodyState extends State<SignInViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<AuthCubit, AuthState>(
-      listener: (context, state) {
-        if (state is AuthLoginSuccess) {
+    return BlocListener<UserDataCubit, UserDataState>(
+      listener: (context, userState) {
+        if (userState is UserDataSuccess) {
           return navigatorPush(context, const HomeView());
         }
-        if (state is AuthLoginFailure) {
-          customShowToast(state.errMess);
-        }
       },
-      builder: (context, state) {
-        var cubit = BlocProvider.of<AuthCubit>(context);
+      child: BlocConsumer<AuthCubit, AuthState>(
+        listener: (context, state) {
+          if (state is AuthLoginSuccess) {
+            uId = CacheHelper.getString(key: 'uid');
+            context.read<UserDataCubit>().fetchUserData(id: uId ?? '');
+          }
+          if (state is AuthLoginFailure) {
+            customShowToast(state.errMess);
+          }
+        },
+        builder: (context, state) {
+          var cubit = BlocProvider.of<AuthCubit>(context);
 
-        return Form(
-          key: formKey,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: ListView(
-              padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.2),
-              children: [
-                const Center(
-                    child: HeaderDescText(
-                  title: 'Sign IN',
-                  subTitle: 'Hi! Welcome back, you \'ve been missed',
-                )),
-                const SizedBox(
-                  height: 40,
-                ),
-                Text('Email', style: AppFonts.bodyText18),
-                CustomTextFormField(
-                    controller: emailController,
-                    hintText: 'Email',
-                    prefixIcon: Icons.email,
-                    keyboardText: TextInputType.emailAddress,
+          return Form(
+            key: formKey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: ListView(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.2),
+                children: [
+                  const Center(
+                      child: HeaderDescText(
+                    title: 'Sign IN',
+                    subTitle: 'Hi! Welcome back, you \'ve been missed',
+                  )),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Text('Email', style: AppFonts.bodyText18),
+                  CustomTextFormField(
+                      controller: emailController,
+                      hintText: 'Email',
+                      prefixIcon: Icons.email,
+                      keyboardText: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'email is required';
+                        }
+                        return null;
+                      }),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text('Password', style: AppFonts.bodyText18),
+                  CustomTextFormField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    prefixIcon: Icons.lock,
+                    suffixIcon: cubit.suffixIcon,
+                    suffixOnPressed: () {
+                      cubit.togglePasswordVisibility();
+                    },
+                    obscureText: cubit.isHidden,
+                    keyboardText: TextInputType.visiblePassword,
                     validator: (value) {
                       if (value!.isEmpty) {
-                        return 'email is required';
+                        return 'Password is required';
                       }
                       return null;
-                    }),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text('Password', style: AppFonts.bodyText18),
-                CustomTextFormField(
-                  controller: passwordController,
-                  hintText: 'Password',
-                  prefixIcon: Icons.lock,
-                  suffixIcon: cubit.suffixIcon,
-                  suffixOnPressed: () {
-                    cubit.togglePasswordVisibility();
-                  },
-                  obscureText: cubit.isHidden,
-                  keyboardText: TextInputType.visiblePassword,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Password is required';
-                    }
-                    return null;
-                  },
-                ),
-                Align(
-                  alignment: AlignmentDirectional.centerEnd,
-                  child: CustomTextButton(
-                    text: 'Forgot password?',
-                    onPressed: () {
-                      navigatorPush(context, const ForgotPasswordView());
                     },
                   ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                ConditionalBuilder(
-                  condition: state is! AuthLoginLoading,
-                  builder: (context) => CustomButton(
-                    text: 'Sign In',
-                    onPressed: () {
-                      if (formKey.currentState!.validate()) {
-                        cubit.login(
-                            email: emailController.text,
-                            password: passwordController.text);
-                      }
-                    },
+                  Align(
+                    alignment: AlignmentDirectional.centerEnd,
+                    child: CustomTextButton(
+                      text: 'Forgot password?',
+                      onPressed: () {
+                        navigatorPush(context, const ForgotPasswordView());
+                      },
+                    ),
                   ),
-                  fallback: (context) => customLoadingIndicator(),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const TextButtonSignUp(),
-              ],
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  ConditionalBuilder(
+                    condition: state is! AuthLoginLoading,
+                    builder: (context) => CustomButton(
+                      text: 'Sign In',
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          cubit.login(
+                              email: emailController.text,
+                              password: passwordController.text);
+                        }
+                      },
+                    ),
+                    fallback: (context) => customLoadingIndicator(),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const TextButtonSignUp(),
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
